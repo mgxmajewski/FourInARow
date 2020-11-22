7ddf94414f5c72bb5a15f4ef0544c2d514dab7aa
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-11-22 19:36:06 +0100

    Creates createSpaces method in Board.js class

--- js/Board.js
+++ js/Board.js
@@ -4,4 +4,26 @@ class Board {
         this.columns = 7;
         this.speces = [];
     }
+
+    /**
+     * Generates 2D array of spaces.
+     * @return  {Array}     An array of space objects
+     */
+
+    createSpaces(){
+        const spaces = [];
+
+        for (let x=0; x<this.columns; x++) {
+            const column = [];
+
+            for (let y = 0; y < this.rows; y++) {
+                const space = new Space(x,y);
+                column.push(space);
+            }
+
+            spaces.push(column);
+        }
+
+        return spaces;
+    }
 }
