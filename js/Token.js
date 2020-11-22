c150d12dc0de19b9dca12b8f5e4e99413875b759
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-11-22 21:05:35 +0100

    Adds render method drawHTMLToken() in Token class

--- js/Token.js
+++ js/Token.js
@@ -1,7 +1,27 @@
 class Token {
-    constructor() {
+    constructor(index, owner) {
         this.owner = owner;
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
+    }
+
+    /**
+     * Gets associated htmlToken.
+     * @return {element} Html element asocciated with token object.
+     */
+    get htmlToken() {
+        return document.getElementById(this.id);
+    }
+
+    /**
+     * Draws new HTML token.
+     */
+    drawHTMLToken(){
+        const token = document.createElement('div');
+        document.getElementById('game-board-underlay').appendChild(token);
+        token.setAttribute('id', this.id);
+        token.setAttribute('class','token');
+        token.style.backgroundColor = this.owner.color;
+
     }
 }
