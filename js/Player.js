65e82c0a0dc5f221c7d7003117ab3bd038d75cfc
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-11-22 18:34:50 +0100

    Develops method in Player class

--- js/Player.js
+++ js/Player.js
@@ -4,10 +4,17 @@ class Player {
         this.id = id;
         this.color = color;
         this.active = active;
-        this.tokens = [];
+        this.tokens = this.createTokens(21);
     }
 
-    createTokens(){
+    createTokens(num){
+        const tokens = [];
 
+        for (let i = 0; i <num; i++) {
+            let token = new Token(i, this);
+            tokens.push(token);
+        }
+
+        return tokens;
     }
 }
