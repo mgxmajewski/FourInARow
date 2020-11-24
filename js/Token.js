228e8e166fedd48b7c70e67399e5e6ca2932b490
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2020-11-24 20:54:31 +0100

    Adds PlayToken() method

--- js/Token.js
+++ js/Token.js
@@ -62,6 +62,7 @@ class Token {
      */
     drop(target, reset) {
         this.dropped = true;
+
         $(this.htmlToken).animate({
             top: (target.y * target.diameter)
         }, 750, 'easeOutBounce', reset);
